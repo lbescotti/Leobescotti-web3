73b108eb4c29e0adc4f63ec64abb2f96b268728f
Leonardo Bescotti <leonardobescotti@Leonardos-MBP.station>
2025-02-25 14:34:40 +0100

    Restored next.config.ts to default settings

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",  // Forces Next.js to generate a static site
-  trailingSlash: true,  // Ensures all URLs end with `/` (important for IPFS)
-  images: {
-    unoptimized: true, // Prevents image optimizations that require a server
-  },
+  /* Default Next.js settings */
 };
 
 export default nextConfig;
