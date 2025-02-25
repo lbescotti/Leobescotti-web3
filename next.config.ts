88e199cd18ff71f18f77a982b7b3ee73b690b0e6
Leonardo Bescotti <leonardobescotti@Leonardos-MBP.station>
2025-02-25 17:18:58 +0100

    Reset next.config.ts to default

--- next.config.ts
+++ next.config.ts
@@ -1,38 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true, // Ensures paths end with `/`
-  images: { unoptimized: true }, // Avoids Next.js image optimization
-  basePath: "/static", // Makes Next.js serve pages from `/static`
-  headers: async () => {
-    return [
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "text/html",
-          },
-          {
-            key: "Content-Disposition",
-            value: 'inline; filename="index.html"',
-          },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/static", // Redirect root to `/static`
-        permanent: true,
-      },
-    ];
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
-
 
