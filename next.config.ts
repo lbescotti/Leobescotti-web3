ce8648c6fd54a1f34b5e0e72b8e8ba9eed4c08fc
Leonardo Bescotti <leonardobescotti@Leonardos-MBP.station>
2025-02-25 18:12:45 +0100

    Build and export static site

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-}
- 
-module.exports = nextConfig
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export", // Tells Next.js to export the app as static files
+  trailingSlash: true, // Ensures URLs end with a slash
+  images: {
+    unoptimized: true, // Avoid image optimization issues when deploying to IPFS
+  },
+};
+
+export default nextConfig;
