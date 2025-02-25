ddd83db25e855b6e8b35177abd4fbd2fddb79f90
Leonardo Bescotti <leonardobescotti@Leonardos-MBP.station>
2025-02-25 14:05:51 +0100

    Fixed Next.js not found error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // Forces Next.js to generate a static site
+  trailingSlash: true,  // Ensures all URLs end with `/` (important for IPFS)
+  images: {
+    unoptimized: true, // Prevents image optimizations that require a server
+  },
 };
 
 export default nextConfig;
